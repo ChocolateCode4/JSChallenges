bfa89c3b4a6b500a83aa608ca6622c69dea05941
ChocolateCode4 <azulinrose@gmail.com>
2020-03-29 15:55:54 +0800

    JSON Metadata

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 window.onload = () => init();
 
-let navbar = document.querySelector("nav"), body = document.querySelector("body"),
-  data1 = document.createElement("h2"),
-  data2 = document.createElement("h2");
-  data2.innerText = "Week 1: Water Physics";
-  data1.innerText = "Week 1: \n Bruh";
-  data1.onclick = (event) => {
-    window.location.href = "week1/index.html";
-  };
-  data2.onclick = (event) => {
-    window.location.href = "week1WaterPhysics/index.html";
-  }
+let navbar = document.querySelector("nav"), body = document.querySelector("body")
+  
 function init() {
-  navbar.appendChild(data1);
-  navbar.appendChild(data2);
-  update();
-}
-
-function update() {
-  window.requestAnimationFrame(update);
+  fetch("metadata/meta.json")
+  .then(res=>res.json())
+  .then(data=>{
+    let challenges = data.challenges;
+    challenges.forEach(chalItem=>{
+      let name = chalItem.name;
+      let loc = chalItem.location;
+      let itemInstance = document.createElement("h2");
+      itemInstance.innerText = name;
+      itemInstance.onclick = () => window.location.href = loc;
+      navbar.appendChild(itemInstance);
+    });
+  });
 }
--- week1/index.js
+++ week1/index.js
@@ -1,106 +1,13 @@
-Window.onload = () => config();
-const game = {
-  state: "loading",
-  objects: []
-};
+window.onload = () => config();
 
 class Canvas {
-  constructor(width, height) {
-    this.width = width;
-    this.height = height;
-    this.color = "#ffffff";
-    this.dom = document.querySelector("canvas");
-    //this.ctx = this.dom.getContext('2d');
-    this.dom.height = height;
-    this.dom.width = width;
-    this.ctx = this.dom.getContext('2d');
-    this.ctx.fillStyle = this.color;
-    this.ctx.fillRect(0,0, this.dom.width, this.dom.height);
-  }
-  
-  clearAll() {
-    this.ctx.clearRect(0,0,this.dom.width, this.dom.height);
-    this.ctx.fillStyle = this.color;
-    this.ctx.fillRect(0, 0, this.dom.width, this.dom.height);
-  }
-}
-
-class TextBox {
-  constructor(text,x,y,w,h,color,colorT){
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
-    this.textX = this.x;
-    this.textY = this.y + this.h/1.15;
-    this.textW = this.h;
-    this.text = text;
-    this.color = color;
-    this.colorT = colorT;
-  }
-  render() {
-      ctx.beginPath();
-      ctx.fillStyle = this.color;
-      ctx.fillRect(this.x, this.y, this.w, this.h);
-      ctx.stroke;
-      ctx.fillStyle = this.colorT;
-      ctx.font = this.textW + "px serif";
-      ctx.fillText(this.text, this.textX, this.textY);
-  }
-  clicked() {
-    console.log("clicked " + this.x +"/"+this.y);
-  }
-  align(position) {
-    switch(position) {
-      case "center":
-        this.textX = this.w/2;
-        break;
-      default:
-        return "Alignment is not possible";
-    }
-  }
-}
-
-let canvas = new Canvas(window.innerWidth, window.innerHeight),
-    ctx = canvas.ctx;
-
-function update() {
-  canvas.clearAll();
-  main();
-  window.requestAnimationFrame(update);
-}
-
-function GUI(scene) {
-  switch(scene) {
-    case "display":
-      let playButton = new TextBox("Play", 0, canvas.height/2, canvas.width, 50, "green", "black");
-      game.state = "menu-start";
-      playButton.align("center");
-      game.objects.push(playButton);
-    break;
-    default:
-      console.log("GUI Fail");
-  }
-}
-
-
-function main() {
-  render();
+ static dom = document.querySelector("canvas");
+ static ctx = Canvas.dom.getContext("2d");
 }
 
 function config() {
-  GUI("display");
-  update();
+  
 }
 
-function render () {
-  game.objects.forEach((objs)=> {
-    objs.render();
-  });
-}
-
-function home() {
-  window.location.href = "jschallenges/index.html";
-}
-
-config();
+// global
+let canvas = Canvas.dom, ctx = Canvas.ctx;
